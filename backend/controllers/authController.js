9304a0c5255e1e5a0274705bc6f400f2b576d547
Zaibbee69 <jahanzaib12569@gmail.com>
2026-07-28 20:17:39 +0500

    Login API working

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,44 @@
 const bcrypt = require("bcryptjs");
 const prisma = require("../prisma/prismaClient")
+const jwt = require("jsonwebtoken")
 
-function loginUser(req, res) {
+async function loginUser(req, res) {
+
+    // Getting content from body
+    const { email, password } = req.body
+
+    // Find the user
+    const user = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    })
+
+    // If user not found
+    if (!user)
+        return res.status("401").json({ message: "Invalid Credentials" })
+
+    // Check if passwords match
+    const passwordsMatch = bcrypt.compare(password, user.passwordHash)
+
+    // If Match
+    if (!passwordsMatch)
+        return res.status("401").json({ message: "Invalid Credentials" })
+
+    // Make a new token for the user
+    const token = jwt.sign({
+        sub: user.id,
+        email: user.email,
+        isAuthor: user.isAuthor
+    },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "7d"
+        })
+
+    // Send token
+    res.json({ token })
+
 
 }
 
@@ -9,8 +46,6 @@ async function signUpUser(req, res) {
 
     // Getting content from body
     const { name, email, password } = req.body
-
-    console.log(req.body)
 
     try {
         // Check that email must be unique
@@ -33,15 +68,11 @@ async function signUpUser(req, res) {
 
         // Return a status telling user was created
         res.status(201).json(user)
-
     }
     catch (err) {
         console.error(err);
         res.status(500).json({ error: "Internal server error" });
     }
-
-
-
 }
 
 module.exports = { loginUser, signUpUser }
