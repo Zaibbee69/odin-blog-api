6120ee0cd0da387806d26f7e602b5f8f8ca0bc69
Zaibbee69 <jahanzaib12569@gmail.com>
2026-07-28 19:42:12 +0500

    SignUp added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,23 +5,43 @@ function loginUser(req, res) {
 
 }
 
-function signUpUser(req, res) {
+async function signUpUser(req, res) {
 
     // Getting content from body
     const { name, email, password } = req.body
 
-    // Getting password hash
-    const passwordHash = await bcrypt.hash(password, 10)
+    console.log(req.body)
 
-    // Creating a new user based on data
-    const user = await prisma.user.create({
-        data: {
-            name, email, passwordHash
-        }
-    })
+    try {
+        // Check that email must be unique
+        const existingUser = await prisma.user.findUnique({
+            where: { email }
+        })
 
-    // Return a status telling user was created
-    req.status(201).json(user)
+        if (existingUser)
+            return res.status(400).json({ error: "Email is already registered" });
+
+        // Getting password hash
+        const passwordHash = await bcrypt.hash(password, 10)
+
+        // Creating a new user based on data
+        const user = await prisma.user.create({
+            data: {
+                name, email, passwordHash
+            }
+        })
+
+        // Return a status telling user was created
+        res.status(201).json(user)
+
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+
+
+
 }
 
 module.exports = { loginUser, signUpUser }
